60b7859404335fbecd1b44b26ea8a6dbc425ceb2
Brendan Lambrecht <lambrecht5083@uwlax.edu>
2025-11-21 13:25:38 -0600

    feat: overhaul Projects page (#227)
    * refactor: reorganize CSS and HTML structure for projects page; update carousel component to use signal API; improve UI Visual
    
    * style: add base styles for headings
    
    * refactor: refactor projects and carousel, streamline layout with inline templates
    
    * refactor: remove PrimeNG dependencies and related configurations, simplify carousel component
    
    * style: improve text alignment and spacing in projects
    
    ---------
    
    Co-authored-by: Brendan Lambrecht <sock460@gmail.com>
    Co-authored-by: Muhammad Fardeen <m.fdeen02@gmail.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,12 @@
 import {ApplicationConfig, provideZonelessChangeDetection} from '@angular/core';
 import {provideClientHydration, withIncrementalHydration} from '@angular/platform-browser';
 import {provideRouter, withPreloading, PreloadAllModules} from '@angular/router';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {routes} from './app.routes';
-import {providePrimeNG} from 'primeng/config';
-import {CODERS_Preset} from '../presets/coders.preset';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideClientHydration(withIncrementalHydration()),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: CODERS_Preset,
-      },
-    }),
   ],
 };
